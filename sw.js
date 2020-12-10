9cd391d39214544c2c6787e825d4743b146b5458
kosalanuwan <kosalanuwan@users.noreply.github.com>
2020-12-10 19:26:53 +0000

    deploy: ea35ff3bd53389c19bb39373dd45ba6a24a1135f

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/vscode-remote-try-bookworm/assets/css/main.css?202012101809",
+        "/vscode-remote-try-bookworm/assets/css/main.css?202012101926",
         "/vscode-remote-try-bookworm/"
       ]);
     })
